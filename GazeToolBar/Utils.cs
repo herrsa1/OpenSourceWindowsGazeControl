6b32bc2caea3c7180cf5661324d88e75619630a7
Samuel Medlock <medlsp1@student.op.ac.nz>
2017-07-24 11:39:32 +1200

    Add utils class to hold basic utilities that don't fit in any specific class

---
+++ GazeToolBar/Utils.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GazeToolBar
+{
+    public class Utils
+    {
+        public static void print(params Object[] toPrint)
+        {
+            print(" ", toPrint);
+        }
+
+        public static void print(String seperator, params Object[] toPrint)
+        {
+            String outString = "";
+            foreach (Object o in toPrint)
+            {
+                outString += o.ToString() + seperator;
+            }
+        }
+    }
+}
